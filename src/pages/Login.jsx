d847923abf548a76d2b8fd4322a3e927db0c4d08
Baharuddin Kasim <kasim.baharuddin@gmail.com>
2023-03-14 13:27:18 +0800

    feat/kasim : add Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,39 +31,7 @@ export default function Login() {
             <a href="#" className="text-white font-bold">SIGN UP</a>
           </div>
         </div>
-
-        
       </div>
-      {/* <div className="flex flex-row justify-evenly">
-        <div className="border">
-          <h4 className='text-2xl text-primary font-bold m-8'>SMUCKER’S</h4>
-        </div>
-
-        <div className="flex flex-col w-{535} items-center border">
-          <h4 className='text-6xl text-white font-bold'>LOGIN</h4>
-          <h4 className='text-4xl text-white font-bold my-5'>To Smucker's</h4>
-
-          <form className="w-full" action="#">
-            <div className="mb-4">
-              <input className="shadow border rounded-full w-1/2 py-2 px-3 bg-formColor text-white leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Email or Phone Number" />
-            </div>
-          </form>
-        </div>
-
-      </div> */}
     </div>
-    // <div className="bg-primaryLogin min-h-screen">
-    //   <div className='flex place-items-end justify-start p-8'>
-    //     <h4 className='text-2xl font-bold text-primary'>SMUCKER’S</h4>
-    //   </div>
-    //   <div className="flex flex-row justify-evenly">
-    //    <div className="absolute left-181px top-159px">
-    //     <img className="object-cover border rounded-full" src="https://cdn-cms.pgimgs.com/news/2020/07/Perpustakaan-di-Rumah-FIMG.jpg" height="50px" alt="" />
-    //    </div>
-    //    <div>
-    //     <h1 className="text-2xl font-bold text-primary">LOGIN</h1>
-    //    </div>
-    //   </div>
-    // </div>
   );
 }
