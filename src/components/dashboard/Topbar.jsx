08db5a22e2ef8c7ccf62be1215336163dbb7ead6
Auzan <istikhoriauzan@gmail.com>
2023-03-16 22:32:56 +0700

    fix/auzan: fix slicing

--- src/components/dashboard/Topbar.jsx
+++ src/components/dashboard/Topbar.jsx
@@ -19,7 +19,7 @@ function Topbar({ handleClick }) {
     <div className='flex items-center justify-between px-5 py-3 mr-5  rounded-lg text-gray-100 bg-gray-800 border-y-zinc-500'>
       <div className='flex items-center gap-x-3'>
         <Bars3Icon
-          className='w-5 h-5 transition duration-200 cursor-pointer hover:text-amber-500'
+          className='w-5 h-5 transition duration-200 cursor-pointer hover:text-indigo-600'
           onClick={() => handleClick()}
         />
         <h2 className='hidden text-lg font-medium capitalize sm:block'>
